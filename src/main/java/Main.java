a153a4e2c29f4c1d94d3c38231d8d58f0eb4559b
Zeynep Yılmaz <102660467+zenepyilmaz@users.noreply.github.com>
2025-04-06 21:49:43 +0300

    created method "createRandomString"
    give lenght as k and watch what its create:D

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
+import helper.InputFileCreator;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("heeellllooğ world");
+		InputFileCreator inpt= new InputFileCreator();
+		inpt.createRandomString(20);
 	}
 
 }
--- src/main/java/helper/InputFileCreator.java
+++ src/main/java/helper/InputFileCreator.java
@@ -1,18 +1,29 @@
 package helper;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class InputFileCreator {
 	
 	public boolean createRandomInputFile (int k) {
 		// k is kmer lenght
 		// to do
+
 		return false;
 	}
 	
+	// hem dna string için hem de kmer üretmek için kullanacağız
 	private String createRandomString(int k) {
-		// hem dna string için hem de kmer üretmek için kullanacağız
-		return null;
+		String chars= "AGCT";
+		Random randomIndex= new Random();
+		StringBuilder randomString= new StringBuilder(); 
+
+		while (randomString.length() < k) { 
+            int index = randomIndex.nextInt(chars.length());
+            randomString.append(chars.charAt(index));
+        }
+		System.out.println(randomString.toString());
+		return randomString.toString();
 	}
 	
 	private String mutation(String kMer, int n) {
