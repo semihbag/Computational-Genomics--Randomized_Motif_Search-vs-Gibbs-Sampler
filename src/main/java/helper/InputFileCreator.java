0eca7d26c69453d5576acb3b7c3589b39a47e1de
semihbag <semih702bag@gmail.com>
2025-04-06 15:14:22 +0300

    init mutation

--- src/main/java/helper/InputFileCreator.java
+++ src/main/java/helper/InputFileCreator.java
@@ -1,9 +1,15 @@
 package helper;
 
+import java.util.ArrayList;
+
 public class InputFileCreator {
 	
 	public boolean createRandomInputFile () {
 		// to do
 		return false;
+	}
+	
+	private void mutation(ArrayList<Character> k_mer, int n) {
+		//
 	}
 }
