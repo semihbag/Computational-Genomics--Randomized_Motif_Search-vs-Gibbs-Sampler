8062e8b2c7af6cb315c3f7c8e9222557451c32d2
Zeynep Yılmaz <102660467+zenepyilmaz@users.noreply.github.com>
2025-04-06 22:24:48 +0300

    deleted uneccsry lines

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,10 @@
-import helper.InputFileCreator;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("heeellllooğ world");
-		InputFileCreator inpt= new InputFileCreator();
-		inpt.createRandomString(20);
+		
 	}
 
 }
