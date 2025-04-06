9eecc886221fd363e3925a47da686b2c936b9629
semihbag <semih702bag@gmail.com>
2025-04-07 00:10:00 +0300

    needNormalize added

--- src/main/java/algorithm/Algorithm.java
+++ src/main/java/algorithm/Algorithm.java
@@ -78,12 +78,21 @@ public abstract class Algorithm {
 	// j: index of kmers (rows)
 	// t: tpye of char(A,C,T,G)
 	private void updateCounts() {
-		int i = 0;
+		int i,j = 0;
 		for (i = 0; i < this.k; i++) {
 			for (String motif : this.motifs) {
 				char nucleotide = motif.charAt(i);
 				int index = getNucleotideIndex(nucleotide);
 				this.counts[index][i]++;
+			}
+		}
+		
+		for (i = 0; i < 4; i++) {
+			for (j = 0; j < this.k; j++) {
+				if (this.counts[i][j] == 0) {
+					needNormalize = true;
+					break;
+				}
 			}
 		}
 	}
