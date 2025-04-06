0e570fc17f0f5157666ef22847ffbaf2e60a65ff
semihbag <semih702bag@gmail.com>
2025-04-06 18:34:15 +0300

    init GibbsSampler

---
+++ src/main/java/algorithm/GibbsSampler.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+public class GibbsSampler extends Algorithm {
+
+	public GibbsSampler(int k) {
+		super(k);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setMotifs() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
