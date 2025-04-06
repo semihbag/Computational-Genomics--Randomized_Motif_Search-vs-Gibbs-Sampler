1247f97101b880401416370d22ba2b000ba6814b
semihbag <semih702bag@gmail.com>
2025-04-06 18:33:05 +0300

    init RandomizedMotifSearch

---
+++ src/main/java/algorithm/RandomizedMotifSearch.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+public class RandomizedMotifSearch extends Algorithm{
+
+	public RandomizedMotifSearch(int k) {
+		super(k);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setMotifs() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
