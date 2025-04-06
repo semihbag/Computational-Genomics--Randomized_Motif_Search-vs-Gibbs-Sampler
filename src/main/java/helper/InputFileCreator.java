2f2563ff697619b1eb31029486d05a353e97a815
Zeynep Yılmaz <102660467+zenepyilmaz@users.noreply.github.com>
2025-04-06 23:19:36 +0300

    created method "mutation"

--- src/main/java/helper/InputFileCreator.java
+++ src/main/java/helper/InputFileCreator.java
@@ -1,7 +1,8 @@
 package helper;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class InputFileCreator {
 	
@@ -17,18 +18,36 @@ public class InputFileCreator {
 		String chars= "AGCT";
 		Random randomIndex= new Random();
 		StringBuilder randomString= new StringBuilder(); 
-
+		int index;
 		while (randomString.length() < k) { 
-            int index = randomIndex.nextInt(chars.length());
+            index = randomIndex.nextInt(chars.length());
             randomString.append(chars.charAt(index));
         }
 		System.out.println(randomString.toString());
 		return randomString.toString();
 	}
 	
-	private String mutation(String kMer, int n) {
+	public String mutation(String kMer, int n) {
 		//
-		return null;
+		//nextInt(kmer.lengt)
+		StringBuilder sb = new StringBuilder(kMer);
+		Random randomIndex= new Random();
+		Set<Integer> markedIndexes = new HashSet<>(); //degisen index tekrar degismesin diye hashset kullanildi
+		int index; 
+		while(markedIndexes.size()<n){  
+			index = randomIndex.nextInt(kMer.length());
+			if(markedIndexes.add(index)){  //eger yeni bir index geldi ise yani yeni bir konum belirlendi
+				char currentChar= kMer.charAt(index);
+				char mutaitedChar;
+                do{
+					mutaitedChar= createRandomChar();
+					//karakteri değiştir dönen karakter!= current ise biter döngü
+				}while(mutaitedChar == currentChar);
+				sb.setCharAt(index, mutaitedChar);
+			}
+		}
+		System.out.println(sb.toString());
+		return sb.toString();
 	}
 	
 	private String insertMutation(String dnaString, String mutatedKMer) {
